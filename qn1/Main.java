25dc1717a86842b0ef443b91b1c1041c428bd4a7
Caleb-LeGrand12 <c.mugisha@alustudent.com>
2021-01-31 21:57:13 +0200

    almost done

--- qn1/Main.java
+++ qn1/Main.java
@@ -10,12 +10,14 @@ import java.util.*;
 class Main {
     
 
-    public static  void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException 
+    {
         int rand;
 
         Queue <String> car = new LinkedList<>();
 
-       for(int i =1 ; i<101; i ++){
+       for(int i =1 ; i<101; i ++)
+       {
            car.add("car "+i);
        }
        
@@ -25,59 +27,108 @@ class Main {
 
      // variable to get a random integer.
 
-    do{
         rand = new Random().nextInt(3);
-    }while(car.size()<0);
-    {
 
-    }
         
-    
-       
-    if("Green" == color[rand])
-    {
-        System.out.println();
-        System.out.println(">The Traffic light is Green <");
-        System.out.println();
-
-        int j =1;
-        while(j<31) 
+        
+        if("Green" == color[rand])
         {
-            //since it takes 30sec when it's green, therefore a car should go once every second
-            Thread.sleep(1000);
-            System.out.println("Removed: " + car.remove());
-            j++;
-        }
+            System.out.println();
+            System.out.println(">The Traffic light is Green <");
+            System.out.println();
+
+            int j =1;
+            while(j<31) 
+            {
+                //since it takes 30sec when it's green, therefore a car should go once every second
+                Thread.sleep(1000);
+                System.out.println("Removed: " + car.remove());
+                j++;
+            }
+            while(car.size()>0)
+            {
+                
+                rand = new Random().nextInt(2); // overwriting rand in order to exhaust all the cars in our queue.
+
+                if("Green" == color[rand])
+                {
+                    System.out.println();
+                    System.out.println("> The Traffic light is Green <");
+                    System.out.println();
+            
+                    int t =1;
+                    while(t<31) 
+                    {
+                        //since it takes 30sec when it's green, therefore a car should go once every second
+                        Thread.sleep(1000);
+                
+                        System.out.println("Removed: " + car.remove());
+                        t++;
+                        break;
+                        
+                    }
+                    
+                }else if("Yellow" == color[rand])
+                {
+                    System.out.println();
+                    System.out.println("> The Traffic light is Yellow <");
+                    System.out.println();
+            
+                    int t =1;
+                    while(t<6) 
+                    {
+                        //since it takes 30sec when it's green, therefore a car should go once every second
+                        Thread.sleep(2000);
+                
+                        System.out.println("Removed: " + car.remove());
+                        t++;
+                        break;
+                        
+                    }
+                    
+                }
+                else
+                {       
+                    System.out.println();
+                    System.out.println(">The Traffic light is Red <");
+                    System.out.println();
+                    System.out.println("You can't move!!");
+                    Thread.sleep(20000);
         
+                }
 
-    }
+            }
+                
+            
 
-    else if("Yellow" == color[rand])
-    // since it takes 10 secs, and rests 2secs, therefore it will only allow five cars 
-
-    {
-        System.out.println();
-        System.out.println(">The Traffic light is Yellow <");
-        System.out.println();
-        int k =1;
-        while(k<6){
-            Thread.sleep(2000);
-            System.out.println("Removed  : " + car.remove());
-            k++;
         }
-    } else
-    {       
-            System.out.println();
-            System.out.println(">The Traffic light is Red <");
-            System.out.println();
-            System.out.println("You can't move!!");
-            Thread.sleep(20000);
 
-    }
+        else if("Yellow" == color[rand])
+        // since it takes 10 secs, and rests 2secs, therefore it will only allow five cars 
+
+        {
+            System.out.println();
+            System.out.println(">The Traffic light is Yellow <");
+            System.out.println();
+            int k =1;
+            while(k<6){
+                Thread.sleep(2000);
+                System.out.println("Removed  : " + car.remove());
+                k++;
+            }
+        } else
+        {       
+                System.out.println();
+                System.out.println(">The Traffic light is Red <");
+                System.out.println();
+                System.out.println("You can't move!!");
+                Thread.sleep(20000);
+
+        }
 
             
-       }
-       
     }
+       
+}
 
 
